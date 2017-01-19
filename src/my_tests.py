f989fba3894877860dd95f4a4b737e2fd74cb40a
Maxime Chéramy <maxime@codingame.com>
2017-01-19 18:50:42 +0100

    add pep515

--- src/my_tests.py
+++ src/my_tests.py
@@ -7,3 +7,9 @@ class MyTests(unittest.TestCase):
         self.assertEqual(fe.compute_sum(1.2345, 0.2, 2), "1.2345 + 0.2 = 1.4")
         self.assertEqual(fe.compute_sum(3.1415, 5, 3), "3.1415 + 5 = 8.14")
         self.assertEqual(fe.compute_sum(3.5, 1.2, 1), "3.5 + 1.2 = 5e+00")
+
+    def testPEP515(self):
+        import numpy as np
+        import underscores_example as ue
+        self.assertEqual(ue.get_element(np.matrix([5120125, 123, 2]), 0, 0), "5_120_125")
+        self.assertEqual(ue.get_element(np.matrix([[13, 25], [1234, 5]]), 1, 0), "1_234")
---
+++ src/underscores_example.py
@@ -0,0 +1,3 @@
+def get_element(a, i, j):
+    el = a.item((i, j))
+    return el  # TODO
